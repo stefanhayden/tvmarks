0af66010c95d65a297b651ad38d0d9c49cdc81f3
Glitch (tvmarks) <none>
2025-04-08 23:23:50 +0000

    🎱📑 Updated with Glitch

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -4,9 +4,9 @@ import { isAuthenticated } from '../session-auth.js';
 const router = express.Router();
 
 router.post('/:id/toggle', isAuthenticated, async (req, res) => {
-  const bookmarksDb = req.app.get('bookmarksDb');
+  const tvshowDb = req.app.get("tvshowDb");
 
-  await bookmarksDb.toggleCommentVisibility(req.params.id);
+  await tvshowDb.toggleCommentVisibility(req.params.id);
 
   return res.redirect(req.get('Referrer'));
 });
